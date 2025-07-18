f36bdd69b0a64698cff9a049f8381afb82e63fb1
FANDY <fandyitdesktop@gmail.com>
2025-07-18 18:39:04 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,15 +2,29 @@
 // KONFIGURASI FIREBASE
 // SUDAH DIISI DENGAN KUNCI ANDA. TIDAK PERLU DIUBAH LAGI.
 // ===================================================================================
-const firebaseConfig = {
-  apiKey: "AIzaSyCOEDrWyOrubRUHXY0AouAiXvR294FcUbQ",
-  authDomain: "data-absen-f5595.firebaseapp.com",
-  projectId: "data-absen-f5595",
-  storageBucket: "data-absen-f5595.appspot.com",
-  messagingSenderId: "357673916959",
-  appId: "1:357673916959:web:2bae1e5ab3f19cb5ca932a",
-  measurementId: "G-DGHTWY3GPG"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCOEDrWyOrubRUHXY0AouAiXvR294FcUbQ",
+    authDomain: "data-absen-f5595.firebaseapp.com",
+    projectId: "data-absen-f5595",
+    storageBucket: "data-absen-f5595.firebasestorage.app",
+    messagingSenderId: "357673916959",
+    appId: "1:357673916959:web:f3b15e00d1f95020ca932a",
+    measurementId: "G-570FNVD28C"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 // ===================================================================================
 
 // Import fungsi-fungsi yang kita butuhkan dari Firebase SDK
