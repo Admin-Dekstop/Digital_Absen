76aa32df4189e50f55d85dd56baa4987df9f256e
FANDY <fandyitdesktop@gmail.com>
2025-07-18 08:32:31 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ const CONFIG = {
     // Masukkan Personal Access Token (PAT) dari akun "bot" GitHub Anda di sini.
     // Token ini akan terlihat di kode sumber, jadi PASTIKAN token ini hanya memiliki
     // akses ke repositori data absensi saja.
-    GITHUB_TOKEN: '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', // Contoh: ghp_xxxxxxxxxxxxxx
+    GITHUB_TOKEN: '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', // Contoh: ghp_xxxxxxxxxxxxxx
 
     // Masukkan path repositori dalam format 'username/nama-repo'.
     // Gunakan username dari akun "bot" Anda.
